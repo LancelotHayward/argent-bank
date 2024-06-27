1655d9750eb1f86be578306dbde4f40f3b6462cd
LancelotHayward <hayward.lance@gmail.com>
2024-06-27 22:55:42 +0200

    Fixed react className and htmlFor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,18 @@ import "./Header.scss"
 
 function Header() {
     return (
-        <nav class="main-nav">
-          <Link class="main-nav-logo" to="/">
+        <nav className="main-nav">
+          <Link className="main-nav-logo" to="/">
             <img
-              class="main-nav-logo-image"
+              className="main-nav-logo-image"
               src="./img/argentBankLogo.png"
               alt="Argent Bank Logo"
             />
-            <h1 class="sr-only">Argent Bank</h1>
+            <h1 className="sr-only">Argent Bank</h1>
           </Link>
           <div>
-            <Link class="main-nav-item" to="/sign-in">
-              <i class="fa fa-user-circle"></i>
+            <Link className="main-nav-item" to="/sign-in">
+              <i className="fa fa-user-circle"></i>
               Sign In
             </Link>
           </div>
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -14,27 +14,27 @@ function Signin() {
       await dispatch(loginThunk())
     }
     return (
-        <main class="main bg-dark">
-            <section class="sign-in-content">
-              <i class="fa fa-user-circle sign-in-icon"></i>
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+              <i className="fa fa-user-circle sign-in-icon"></i>
               <h1>Sign In</h1>
               <form>
-                <div class="input-wrapper">
-                  <label for="username">Username</label>
+                <div className="input-wrapper">
+                  <label htmlFor="username">Username</label>
                   <input type="text" id="username" />
                 </div>
-                <div class="input-wrapper">
-                  <label for="password">Password</label>
+                <div className="input-wrapper">
+                  <label htmlFor="password">Password</label>
                   <input type="password" id="password" />
                 </div>
-                <div class="input-remember">
+                <div className="input-remember">
                   <input type="checkbox" id="remember-me" />
-                  <label for="remember-me">Remember me</label>
+                  <label htmlFor="remember-me">Remember me</label>
                 </div>
                 {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-                <Link class="sign-in-button" to="/dashboard">Sign In</Link>
+                <Link className="sign-in-button" to="/dashboard">Sign In</Link>
                 {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
-                <button class="sign-in-button" onClick={manageSignin} type="button">Sign In</button>
+                <button className="sign-in-button" onClick={manageSignin} type="button">Sign In</button>
                 {/* <!--  --> */}
               </form>
             </section>
